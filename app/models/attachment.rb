e0033c1c39b722e177910fca5f2e5f3c36a9ff91
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-05 16:54:04 +0530

    Add enum in attachment model update schema file with new migrations

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,3 +1,4 @@
 class Attachment < ApplicationRecord
     belongs_to :service
+    enum attachment_type: {image:0,video:1}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_115806) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_05_110855) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "attachments", force: :cascade do |t|
-    t.string "image_url"
+    t.string "attachment_url"
     t.bigint "service_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "attachment_type"
     t.index ["service_id"], name: "index_attachments_on_service_id"
   end
 
   create_table "features", force: :cascade do |t|
-    t.string "feature_name"
+    t.string "name"
     t.bigint "service_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -33,7 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_115806) do
   create_table "services", force: :cascade do |t|
     t.string "service_name"
     t.string "service_info"
-    t.string "video_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

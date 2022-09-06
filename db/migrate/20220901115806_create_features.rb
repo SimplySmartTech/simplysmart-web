4d63599d673db77c495613e6b92481145af5c9d2
Chinmay891 <78458328+Chinmay891@users.noreply.github.com>
2022-09-06 17:53:01 +0530

    Db/models service attachment feature (#2)
    * Add Service model and create migration for services table
    
    * Add Attachment model and create migration for attachments table
    
    * Add Feature model and create migration for features table
    
    * Add migration files
    
    * Add schema file
    
    * Add migrations for
     - update the column name of features table and attachments table
     - drop video url column from services table
     - add column to attachments table with enum type
    
    * Add enum in attachment model
    update schema file with new migrations
    
    * Changed the has_many value to plural form
    
    * Rename existing migrations

---
+++ db/migrate/20220901115806_create_features.rb
@@ -0,0 +1,9 @@
+class CreateFeatures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :features do |t|
+      t.string :feature_name
+      t.references :service, foreign_key: true
+      t.timestamps
+    end
+  end
+end

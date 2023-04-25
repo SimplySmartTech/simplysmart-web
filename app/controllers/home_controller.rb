86625a7cba496a1912261f0b0b4d2dc2c04ff946
Vishwajeetsingh Desurkar <vishwajeetsinghd@gmail.com>
2023-04-25 13:06:45 +0530

    Add about_us, contact_us, solutions, clients pages with routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def about_us
+  end
+
+  def solutions
+  end
+
+  def clients
+  end
+
+  def contact_us
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  get '/about_us' => "home#about_us"
+  get '/solutions' => "home#solutions"
+  get '/clients' => "home#clients"
+  get 'contact_us' => "home#contact_us"
 end
